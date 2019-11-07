96247b5284ac3b69f5c5939ec6f1e3d32a2ef51a
paul77uk <paul77uk@gmail.com>
2019-11-07 18:13:53 +0000

    Update contacts_list_page.dart

--- contacts_list_page.dart
+++ contacts_list_page.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-class ContactsListPage extends StatelessWidget {
+class ListViewBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contacts'),
+        title: Text('ListView Builder'),
       ),
       body: Center(
         child: ListView.builder(
           itemCount: 30,
           itemBuilder: (context, index) {
             return Text(
-              'Contact test',
+              'test text',
               style: TextStyle(fontSize: 30),
             );
           },
